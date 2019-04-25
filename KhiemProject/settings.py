2fe6302e3b429503fc6217359fa03b63e9fa9dc4
duy <trinhphuduy102@gmail.com>
2019-04-25 21:08:23 +0700

    update

--- KhiemProject/settings.py
+++ KhiemProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'muhkuv!sh#6m+=94s@y+$np9f_bvz=-cn%^st7ooi)1nf_*!g)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'khiemproject.pythonanywhere.com', '192.168.0.120']
+ALLOWED_HOSTS = ['localhost', 'khiemproject.pythonanywhere.com', '192.168.0.120', 'iotprojecttdtu.pythonanywhere.com']
 
 # Application definition
 
