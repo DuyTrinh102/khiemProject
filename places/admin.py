a1feb2c15777f61171cb8750027d5e9d6921acc7
duy <trinhphuduy102@gmail.com>
2019-04-25 21:14:53 +0700

    update

--- places/admin.py
+++ places/admin.py
@@ -20,6 +20,7 @@ class SensorInlines(admin.TabularInline):
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
 	list_display = ('name', 'address', 'owner')
+	fields = ('place_code', 'name', 'address', 'owner')
 	inlines = [
 		LoadInlines,
 		SensorInlines
